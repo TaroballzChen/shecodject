6ffbeb43164d9485b42a4749121bc279cb302c3f
curtis992250 <curtis992250@gmail.com>
2017-11-13 11:23:50 +0800

    Update listener.py

--- module/listener.py
+++ module/listener.py
@@ -16,7 +16,6 @@ class msflistener(Moduleobject):
         if self.values['source']:
             os.system('msfconsole -r %s'%self.values['source'])
         else:
-            os.system('msfconsole -r %s')
             os.system('msfconsole -x "use multi/handler;set PAYLOAD %s;set LHOST %s;set LPORT %s;exploit -j -z"'
                       % (self.values['payload'],
                          self.values['IP'],
