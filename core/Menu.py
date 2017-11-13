388b10b9a30b0b60a8535cc8f670b1c8468834e8
curtis992250 <curtis992250@gmail.com>
2017-11-13 20:01:48 +0800

    Update Menu.py

--- core/Menu.py
+++ core/Menu.py
@@ -3,7 +3,7 @@ class Menu:
         self.options = []
         self.options.append(('scc'  , 'set custom msf_payload raw file path'))
         self.options.append(('mpr' , 'create common msf payload raw file'))
-        self.options.append(('obf'  , 'obfuscate shellcode(only for common reverse_tcp)'))
+        self.options.append(('obf'  , 'obfuscate code(only for common rev_tcp)'))
         self.options.append(('exe' , 'pack python_file to exe by pyinstaller'))
         self.options.append(('msf' , 'start listener with msf'))
         self.options.append(('exit', 'bye-bye'))
