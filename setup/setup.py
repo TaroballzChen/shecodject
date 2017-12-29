d156071927650458c72034e2ed6e1059a3b42fc1
curtis992250 <curtis992250@gmail.com>
2017-12-30 01:52:30 +0800

    Add files via upload

--- setup/setup.py
+++ setup/setup.py
@@ -1,8 +1,10 @@
 import os
 
+os.system('dpkg --add-architecture i386 && apt-get update && apt-get install wine32')
 os.system('sudo apt-get update')
 os.system('sudo apt-get install wine')
 os.system('sudo apt-get install winetricks')
 os.system('pip install distorm3')
-#os.system('wine msiexec /i python-3.4.3.msi /L*v log.txt')
-#os.system('wine /root/.wine/drive_c/Python34/python.exe /root/.wine/drive_c/Python34/Scripts/pip.exe install pyinstaller')
+os.system('WINEPREFIX="$HOME/prefix32" WINEARCH=win32 wine wineboot')
+os.system('WINEPREFIX="$HOME/prefix32"  wine msiexec /i python-3.4.3.msi')
+os.system('WINEPREFIX="$HOME/prefix32" wine /root/prefix32/drive_c/Python34/python.exe /root/prefix32/drive_c/Python34/Scripts/pip.exe install pyinstaller')
